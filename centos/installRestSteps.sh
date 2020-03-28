0c42e13f519c0fb78044a3d48f14ac1471c91aed
sanjibbehera <51856169+sanjibbehera@users.noreply.github.com>
2020-03-28 23:20:17 +0530

    Delete installRestSteps.sh
    installRestSteps.sh was not required after the scripts were restructed.

--- centos/installRestSteps.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo mkdir -p $HOME/.kube && sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && sudo chown $(id -u):$(id -g) $HOME/.kube/config
-sudo kubectl apply -f https://docs.projectcalico.org/v3.11/manifests/calico.yaml
